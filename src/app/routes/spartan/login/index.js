640998fe355ef1eb8f069009f0041b0288934d9c
Henrique Pessolato <henrique.pessolato@quidde.com.br>
2018-03-22 17:24:41 -0300

    Criacao do Login

---
+++ src/app/routes/spartan/login/index.js
@@ -0,0 +1,15 @@
+export default {
+  path: 'spartan',
+  // component: require('../../../components/common/Layout').default,
+  childRoutes: [
+    {
+      path: 'login',
+      getComponent(nextState, cb){
+        System.import('./containers/Login').then((m)=> {
+          cb(null, m.default)
+        })
+      }
+    },
+  ]
+
+};
