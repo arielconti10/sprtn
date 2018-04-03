dda0fab97db20d9d2403f37c91e501b1a3f019d3
Thiago <thiago.carnaes@gmail.com>
2018-04-03 17:23:10 -0300

    update route lists

--- src/app/containers/Sector/SectorList.js
+++ src/app/containers/Sector/SectorList.js
@@ -1,13 +1,6 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import { Card, CardHeader, CardFooter, CardBody, Button} from 'reactstrap';
-import BootstrapTable from 'react-bootstrap-table-next'
-import paginationFactory from 'react-bootstrap-table2-paginator'
-import filterFactory, { textFilter, Comparator, selectFilter } from 'react-bootstrap-table2-filter';
-
-import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css'
-import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css'
-
 
 import axios from '../../common/axios';
 import GridApi from '../../common/GridApi';
@@ -23,7 +16,7 @@ class SectorList extends Component {
                 </CardHeader>
                 <CardBody>  
                     <p>
-                        <NavLink to={this.props.match.url + "/form"} exact><Button color='primary' ><i className="fa fa-plus-circle"></i> Adicionar</Button></NavLink>                        
+                        <NavLink to={this.props.match.url + "/novo"} exact><Button color='primary' ><i className="fa fa-plus-circle"></i> Adicionar</Button></NavLink>                        
                     </p>
                     <div>
                     <GridApi
