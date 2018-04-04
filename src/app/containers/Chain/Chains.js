e2c027b7259838e6f612ded4d34c3cbb18fab158
FTD-NT\tercmt-tiago <tiago.r06@gmail.com>
2018-04-04 07:36:06 -0300

    ChainForm

--- src/app/containers/Chain/Chains.js
+++ src/app/containers/Chain/Chains.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
-import {Row, Col, Card, CardHeader, CardFooter, CardBody, Button} from 'reactstrap';
+import { Route, Switch } from 'react-router-dom';
+import { Row, Col, Card, CardHeader, CardFooter, CardBody, Button } from 'reactstrap';
 
 import ChainList from './ChainList';
 import ChainForm from './ChainForm';
@@ -10,9 +10,12 @@ class Chains extends Component {
     render() {
         return (
             <Row>
-                <Col xs="12" sm="12" md="12">                         
-                    <Route path='/cadastro/redes' exact component={ChainList} />
-                    <Route path='/cadastro/redes/form' component={ChainForm} />                        
+                <Col xs="12" sm="12" md="12">
+                    <Switch>
+                        <Route path='/cadastro/redes' exact component={ChainList} />
+                        <Route path='/cadastro/redes/novo' exact component={ChainForm} />
+                        <Route path='/cadastro/redes/:id' component={ChainForm} />
+                    </Switch>
                 </Col>
             </Row>
         )
