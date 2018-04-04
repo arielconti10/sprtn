8e5517cc125fce166193a732c358fbd4f8ac1552
FTD-NT\tercmt-tiago <tiago.r06@gmail.com>
2018-04-04 09:35:36 -0300

    remove form from states

--- src/app/containers/State/States.js
+++ src/app/containers/State/States.js
@@ -3,7 +3,6 @@ import { Route } from 'react-router-dom';
 import {Row, Col, Card, CardHeader, CardFooter, CardBody, Button} from 'reactstrap';
 
 import StateList from './StateList';
-import StateForm from './StateForm';
 
 class States extends Component {
 
@@ -11,8 +10,7 @@ class States extends Component {
         return (
             <Row>
                 <Col xs="12" sm="12" md="12">                         
-                    <Route path='/cadastro/estados' exact component={StateList} />
-                    <Route path='/cadastro/estados/form' component={StateForm} />                        
+                    <Route path='/cadastro/estados' exact component={StateList} />                      
                 </Col>
             </Row>
         )
