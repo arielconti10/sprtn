defeae047230e78dee1d0f7f56932149d80fb3e6
FTD-NT\tercmt-tiago <tiago.r06@gmail.com>
2018-04-04 07:52:30 -0300

    DisciplineForm

--- src/app/containers/Discipline/Disciplines.js
+++ src/app/containers/Discipline/Disciplines.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
-import {Row, Col, Card, CardHeader, CardFooter, CardBody, Button} from 'reactstrap';
+import { Route, Switch } from 'react-router-dom';
+import { Row, Col, Card, CardHeader, CardFooter, CardBody, Button } from 'reactstrap';
 
 import DisciplineList from './DisciplineList';
 import DisciplineForm from './DisciplineForm';
@@ -10,9 +10,12 @@ class Disciplines extends Component {
     render() {
         return (
             <Row>
-                <Col xs="12" sm="12" md="12">                         
-                    <Route path='/cadastro/disciplinas' exact component={DisciplineList} />
-                    <Route path='/cadastro/disciplinas/form' component={DisciplineForm} />                        
+                <Col xs="12" sm="12" md="12">
+                    <Switch>
+                        <Route path='/cadastro/disciplinas' exact component={DisciplineList} />
+                        <Route path='/cadastro/disciplinas/novo' exact component={DisciplineForm} />
+                        <Route path='/cadastro/disciplinas/:id' component={DisciplineForm} />
+                    </Switch>
                 </Col>
             </Row>
         )
