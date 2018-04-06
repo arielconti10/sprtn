e5edea9db96ad0144c5ca022300e1ec3441376d9
Henrique Pessolato <henrique.pessolato@quidde.com.br>
2018-04-06 10:27:21 -0300

    Alteracao no Jenkinsfile

--- shipitfile.js
+++ shipitfile.js
@@ -7,7 +7,7 @@ module.exports = shipit => {
   shipit.initConfig({
     default: {
       deployTo: '/var/www/html/ftdspartanf',
-      repositoryUrl: 'https://git@bitbucket.org/editora-ftd/ftdspartanf.git',
+      repositoryUrl: 'git@bitbucket.org:editora-ftd/ftdspartanf.git',
     //   repositoryUrl: 'git@bitbucket.org:editora-ftd/ftdspartanf.git',
       branch: 'staging',
       keepReleases: 3
@@ -26,5 +26,7 @@ module.exports = shipit => {
   });
 
 }
+
+//set('repository', 'git@bitbucket.org:editora-ftd/ftdspartanb.git');
 
 //repositoryUrl: 'ssh://git@bitbucket.org:editora-ftd/ftdspartanf.git',
