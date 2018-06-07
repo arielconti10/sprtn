52a280323325906fca601d3097ee26d2cb34c0a7
FTD-NT\tercmt-tiago <tiago.r06@gmail.com>
2018-06-07 15:46:29 -0300

    implemented only the subsidiaryand sector of the current user in the market share selects

--- src/app/containers/MarketShare/MarketShare.js
+++ src/app/containers/MarketShare/MarketShare.js
@@ -1,150 +1,128 @@
 import React, { Component } from 'react';
 import { Col, Row, Card, CardBody, CardHeader } from 'reactstrap';
 import Select from 'react-select';
-import { RingLoader } from 'react-spinners';
 import { Chart } from 'react-google-charts';
 import axios from '../../../app/common/axios';
 import { canUser } from '../../common/Permissions';
 
-const apiPost = 'marketshare?filter[key]=_EDITORAS_CONSOLIDADO&filter[year]=2016';
+import './MarketShare.css'
 
-const apiPostSubsidiary = 'subsidiary';
-const apiPostSector = 'sector';
+const apiUserCurrent = 'user/current';
 
 const pieOptions = {
-    pieHole: 0.5,
-    slices: [{ color: '#009de8', }, { color: '#FD0006', }, { color: '#1aaf5d', }, { color: '#f45b00', }, {
-        color: '#8e0000',
-    }, { color: '#7D7D7D', },],
-    tooltip: {
-        showColorCode: true,
-    }
+    pieHole: 0.3,
+    slices: [{ color: '#009de8', }, { color: '#FD0006', }, { color: '#1aaf5d', }, { color: '#f45b00', }, { color: '#8e0000', }, { color: '#7D7D7D', }],
+    tooltip: { showColorCode: true }
 };
-
-const vltypes = [{ value: 'ESCOLA', label: 'ESCOLA' }, { value: 'SECRETARIA', label: 'SECRETARIA' }];
 
 class MarketShare extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            ring_load: false,
+
+            user_subsidiary: 0,
+            user_sector: 0,
+
             subsidiary_param: 0,
             sector_param: 0,
             type_param: 0,
+            year_param: '2016',
 
-            datasubsidiary: [],
+            data_values: [{ value: 'ESCOLA', label: 'ESCOLA' }, { value: 'SECRETARIA', label: 'SECRETARIA' }],
+            data_subsidiary: [],
             data_sector: [],
-            marketshare: [],
-            schoolId: '0',
-            data_publisher: [],
-            show: '',
+            data_publisher: [['EDITORAS', '%']]
         };
+
         this.handleChangeSubsidiary = this.handleChangeSubsidiary.bind(this);
         this.chargeChart = this.marketShareChart.bind(this);
+        this.clearData = this.clearData.bind(this);
     }
 
     componentWillMount() {
         canUser('indicator.view', this.props.history, "view");
-        this.setState({
-            ringLoad: false
-        });
+    }
+
+    clearData() {
+        this.setState({ data_publisher: [['EDITORAS', '%']] });
     }
 
     loadSubsidiary() {
-        axios.get(`${apiPostSubsidiary}`).then(response => {
-            let subsidiary = response.data.data;
-            var Data = [];
-            subsidiary.map((item, i) => {
-                Data.push({ value: item.id, label: item.name, sectors: item.sectors });
-            });
-            this.setState({ datasubsidiary: Data });
+        axios.get(`${apiUserCurrent}`).then(response => {
+            let user = response.data.data;
+            let subsidiary = user.subsidiary;
+            let data = [];
+
+            data.push({ value: subsidiary.id, label: `${subsidiary.code} - ${subsidiary.name}`, sectors: user.sector });
+
+            this.setState({ data_subsidiary: data });
         }).catch(err => console.log(4, err));
 
-        this.setState({
-            ringLoad: false
-        });
+        this.setState({ ring_load: false });
     }
 
     loadSector(sectors) {
-        var Data = [];
-        Data.push({ value: 'TODOS', label: 'TODOS' })
-        sectors.map(item => {
-            Data.push({ value: item.id, label: item.name });
-        });
-        this.setState({ data_sector: Data });
+        this.setState({ data_sector: [{ value: sectors.id, label: sectors.name }] });
     }
 
     marketShareChart() {
+        this.setState({ ring_load: true });
 
-        let initDataP = [['Editoras', '%']];
-        let initDataC = [['Coleções', '%', { 'role': 'style' }], ['', 0, '']];
-        this.setState({ ringLoad: true, data_publisher: initDataP });
+        let urlPost = `marketshare?filter[key]=${this.state.type_param}_EDITORAS_CONSOLIDADO&filter[year]=${this.state.year_param}`;
 
-        if (this.state.schoolId !== undefined) {
+        axios.get(`${urlPost}`).then(response => {
+            let marketShare = response.data.data;
+            let publishers = this.state.data_publisher;
+            let pubFTD = null;
+            let aux = 0;
+            let order = [];
+            let vlOthers = 0;
 
-            let urlPost = 'marketshare?filter[key]=' + this.state.type_param + '_EDITORAS_CONSOLIDADO&filter[year]=2016';
+            marketShare.sort((a, b) => a.value - b.value);
 
-            axios.get(`${urlPost}`).then(response => {
+            marketShare.map((item, i) => {
+                let register = item.key.split(':');
+                let key = register[0];
+                let sector = register[1];
+                let label = register[2];
 
-                let marketshare = response.data.data;
-                marketshare.sort((a, b) => a.value - b.value);
+                if (this.state.subsidiary_param == item.subsidiary_id && sector == this.state.sector_param) {
+                    if (aux !== item.value) {
+                        order.push(item);
+                        aux = item.value;
+                    }
+                }
+            });
 
-                let publishers = this.state.data_publisher;
-                let pubFTD = null;
-                let aux = 0;
-                let order = [];
-                let vlother = 0;
+            let data_order = order.reverse();
 
-                marketshare.map((item, i) => {
-                    let register = item.key.split(':');
-                    let key = register[0];
-                    let sector = register[1];
-                    let label = register[2];
+            data_order.map((item, i) => {
+                let register = item.key.split(':');
+                let key = register[0];
+                let sector = register[1];
+                let label = register[2];
+
+                if (i <= 4) {
                     if (this.state.subsidiary_param == item.subsidiary_id && sector == this.state.sector_param) {
-                        if (aux !== item.value) {
-                            order.push(item);
-                            aux = item.value;
+                        if (label.search("FTD") !== -1) {
+                            pubFTD = [label, item.value];
+                        } else {
+                            publishers.push([label, item.value,]);
                         }
                     }
-                });
-
-                let data_order = order.reverse();
-                let cont = 0;
-
-                data_order.map((item, i) => {
-                    let register = item.key.split(':');
-                    let key = register[0];
-                    let sector = register[1];
-                    let label = register[2];
-
-                    if (cont <= 4) {
-                        if (this.state.subsidiary_param == item.subsidiary_id && sector == this.state.sector_param) {
-                            if (label.search("FTD") !== -1) {
-                                pubFTD = [label, item.value];
-                            } else {
-                                publishers.push([label, item.value,]);
-                            }
-                        }
-                    } else {
-                        vlother += item.value;
-                    }
-                    cont = cont + 1
-                });
-                if (vlother !== "") {
-                    publishers.push(["Outros", vlother]);
+                } else {
+                    vlOthers += item.value;
                 }
-                if (pubFTD) {
-                    publishers.splice(1, 0, pubFTD);
-                }
+            });
 
-                this.setState({
-                    data_publisher: publishers,
-                    ringLoad: false
-                });
+            if (vlOthers !== "") publishers.push(["OUTROS", vlOthers]);
 
-            }).catch(err => console.log(4, err));
-        } else {
-            this.setState({ ringLoad: false });
-        }
+            if (pubFTD) publishers.splice(1, 0, pubFTD);
+
+            this.setState({ data_publisher: publishers, ring_load: false });
+
+        }).catch(err => console.log(4, err));
     }
 
     handleChangeSubsidiary = (selectedOption) => {
@@ -152,14 +130,15 @@ class MarketShare extends Component {
         values.subsidiary_param = selectedOption.value;
         this.setState({ values });
         this.loadSector(selectedOption.sectors);
-        this.setState({ sector_param: '', show: '' });
+        this.clearData();
+        this.setState({ sector_param: '' });
     }
 
     handleChangeSetor = (selectedOption) => {
         const values = this.state;
         values.sector_param = selectedOption.value;
         this.setState({ values });
-        this.setState({ show: '1' })
+        this.clearData();
         this.marketShareChart(sector_param);
     }
 
@@ -167,20 +146,23 @@ class MarketShare extends Component {
         const values = this.state;
         values.type_param = selectedOption.value;
         this.setState({ values });
-        this.setState({ subsidiary_param:'' ,sector_param: '', show: '' });
+        this.setState({ subsidiary_param: '', sector_param: '' });
+        this.clearData();
         this.loadSubsidiary();
     }
 
     render() {
+        const { data_publisher, ring_load, data_values, data_subsidiary, subsidiary_param, data_sector, sector_param, type_param } = this.state;
 
-        const { data_publisher, ringLoad, datasubsidiary, subsidiary_param, data_sector, sector_param, selectedOption, type_param } = this.state;
         return (
-            <div className="animated fadeIn">
-                <RingLoader
-                    color={'#123abc'}
-                    loading={ringLoad}
-                    margin='50px'
-                />
+            <div>
+                {ring_load == true &&
+                    <div className="loader-marketshare"> 
+                        <div className="backLoading"> 
+                            <div className="load"><img src="https://www.ipswitch.com/library/img/loading.gif" /></div> 
+                        </div> 
+                    </div> 
+                } 
                 <Row>
                     <Col md="2">
                         <label>Tipo</label>
@@ -191,33 +173,32 @@ class MarketShare extends Component {
                             clearable={false}
                             value={type_param}
                             onChange={(selectedOption) => this.handleChangeType(selectedOption)}
-                            options={vltypes}
+                            options={data_values}
                             placeholder="Não há registros"
                         />
                     </Col>
 
-
-
-                    <Col md="2">
+                    <Col md="3">
                         <label>Filial</label>
 
                         <Select
                             name="subsidiary_param"
                             id="subsidiary_param"
-                            disabled={!(datasubsidiary.length > 0)}
+                            disabled={!(data_subsidiary.length > 0)}
                             clearable={false}
                             value={subsidiary_param}
                             onChange={(selectedOption) => this.handleChangeSubsidiary(selectedOption)}
-                            options={datasubsidiary}
+                            options={data_subsidiary}
                             placeholder="Não há registros"
                         />
                     </Col>
+
                     <Col md="2">
                         <label>Setor</label>
                         <Select
                             name="sector_param"
                             id="sector_param"
-                            disabled={!(data_sector.length > 1)}
+                            disabled={!(data_sector.length > 0)}
                             clearable={false}
                             value={sector_param}
                             onChange={this.handleChangeSetor}
@@ -227,29 +208,29 @@ class MarketShare extends Component {
                     </Col>
                 </Row>
                 <br />
-                {this.state.show == '1' && (
-                    <Row>
-                        <Col md="12">
-                            <Card>
-                                <CardHeader>
-                                    <span><strong>Editoras </strong></span>
-                                </CardHeader>
-                                <CardBody>
-                                    <Chart
-                                        chartType="PieChart"
-                                        data={data_publisher}
-                                        options={pieOptions}
-                                        graph_id="PieChart"
-                                        width="100%"
-                                        height="400px"
-                                        legend_toggle
-                                    />
-                                </CardBody>
-                            </Card>
-                            <br />
-                        </Col>
-                    </Row>
-                )}
+
+                <Row>
+                    <Col md="12">
+                        <Card>
+                            <CardHeader>
+                                <span><strong>EDITORAS </strong></span>
+                            </CardHeader>
+                            <CardBody>
+                                <Chart
+                                    chartType="PieChart"
+                                    data={data_publisher}
+                                    options={pieOptions}
+                                    graph_id="PieChart"
+                                    width="100%"
+                                    height="400px"
+                                    legend_toggle
+                                />
+                            </CardBody>
+                        </Card>
+                        <br />
+                    </Col>
+                </Row>
+
             </div>
         )
     }
