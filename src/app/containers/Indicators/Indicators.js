ddf99d5c79074b48334f24ee4d511568c8c09a4f
Henrique Pessolato <henrique.pessolato@quidde.com.br>
2018-06-21 20:21:45 -0300

    904 - Indicadores de usuarios - melhoria nos filtros

--- src/app/containers/Indicators/Indicators.js
+++ src/app/containers/Indicators/Indicators.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-import { Nav, NavItem, NavLink, TabContent, TabPane, Row, Col } from 'reactstrap';
 import axios from '../../../app/common/axios';
 import { formatNumber } from '../../../app/common/FormatHelper';
 import { mapPieChart } from '../../../app/common/ChartHelper';
 
-import { canUser } from '../../common/Permissions';
 import IndicatorNumber from './IndicatorNumber';
 import './Indicators.css';
-import classnames from 'classnames';
 import PieChartComponent from './PieChartComponent';
+import { Async } from 'react-select';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+
+import Select from 'react-select';
 
 class Indicators extends Component {
     constructor(props) {
@@ -20,6 +21,11 @@ class Indicators extends Component {
             total_students: '00',
             total_contacts: '00',
             total_action: '00',
+            school_types: [],
+            search_options: [],
+            school_type_id: '',
+            action_type_initial: [],
+            actions_initial: [],
             data_actions: [],
             data_school_type: [],
             data_student_type: [],
@@ -34,21 +40,32 @@ class Indicators extends Component {
                     alignment: 'center'
                 } 
             },
+            ring_load: false,
             msg_error: ''
 
         };
 
+        this.handleChangeSchoolType = this.handleChangeSchoolType.bind(this);
         this.getActionsRealized = this.getActionsRealized.bind(this);
     }
 
     //o "callback hell" será aprimorado após estudo de promises
     //foi utilizado por ser assíncrono, e se chamado de modo síncrono, nāo produz os valores desejados
-    componentWillMount() {
+    componentDidMount() {
+        this.setState( { ring_load : true });
+
+        this.setState({school_type_id: [
+            { "value": "PARTICULAR", "label": "Particular" },
+            { "value": "PUBLICO", "label": "Público" },
+            { "value": "SECRETARIA", "label": "Secretaria" }
+        ]});
         // canUser('indicator.view', this.props.history, "view");
         this.requestTotal("indicator/school/total", "total_schools", function () {
             this.requestTotal("indicator/student/total", "total_students", function () {
                 this.requestTotal("indicator/school/contact", "total_contacts", function () {
-                    this.requestTotal("indicator/action/total", "total_action", function () { });
+                    this.requestTotal("indicator/action/total", "total_action", function () { 
+                        this.getCoverage();
+                    });
                 });
             });
         });
@@ -57,8 +74,9 @@ class Indicators extends Component {
         this.getSchoolTypes();
         this.getStudentTypes();
         this.getActionTypes();
-        this.getStudentLevel();
-        this.getCoverage();
+        this.getStudentLevel();    
+        
+        // this.setState({ ring_load : false });
     }
 
     toggle(tab) {
@@ -84,20 +102,26 @@ class Indicators extends Component {
                         return item.year === actual_year
                     });
 
-                    const not_coverage = this.state.total_schools - results_year.total;
+                    const total_schools = this.state.total_schools.replace(/\./g,'');
+
+                    const not_coverage = parseFloat(total_schools) - parseFloat(results_year.total);
 
                     const array_chart = [
-                        { "name": "Coberto", "total": results_year.total },
+                        { "name": "Coberto", "total": parseFloat(results_year.total) },
                         { "name": "Nāo Coberto", "total": not_coverage },
                     ];
 
-                    const data_coverage = mapPieChart("Ações", "name", "total", array_chart);
-                    this.setState({ data_coverage });
+                    return array_chart;
                 } else {
                     const array_chart = [];
-                    const data_coverage = mapPieChart("Ações", "name", "total", array_chart);
-                    this.setState({ data_coverage });
+                    return array_chart;
                 }
+            })
+            .then(chart_return => {
+                const data_coverage = mapPieChart("Ações", "name", "total", chart_return);
+                this.setState({ data_coverage, ring_load: false });
+
+                
             })
             .catch(error => {
                 const response = error.response;
@@ -114,7 +138,8 @@ class Indicators extends Component {
             .then(res => {
                 const result = res.data.data;
                 const data_actions = mapPieChart("Ações", "name", "total", result);
-                this.setState({ data_actions });
+
+                this.setState({ data_actions, actions_initial: data_actions });
             })
             .catch(error => {
                 const response = error.response;
@@ -131,7 +156,10 @@ class Indicators extends Component {
             .then(res => {
                 const result = res.data.data;
                 const data_school_type = mapPieChart("Tipos de Escola", "school_type", "total", result);
-                this.setState({ data_school_type });
+
+                this.groupBySchool(this.state.school_type_id, data_school_type, "data_school_type");
+
+                // this.setState({ data_school_type } );
             })
             .catch(error => {
                 const response = error.response;
@@ -148,7 +176,9 @@ class Indicators extends Component {
             .then(res => {
                 const result = res.data.data.total;
                 const data_student_type = mapPieChart("Tipos de Escola", "school_type", "total", result);
-                this.setState({ data_student_type });
+
+                this.groupBySchool(this.state.school_type_id, data_student_type, "data_student_type");
+                // this.setState({ data_student_type });
             })
             .catch(error => {
                 const response = error.response;
@@ -165,7 +195,7 @@ class Indicators extends Component {
             .then(res => {
                 const result = res.data.data;
                 const data_action_type = mapPieChart("Tipos de Escola", "school_type", "total", result);
-                this.setState({ data_action_type });
+                this.setState({ data_action_type, action_type_initial: data_action_type });
             })
             .catch(error => {
                 const response = error.response;
@@ -235,15 +265,184 @@ class Indicators extends Component {
         return true;
     }
 
+
+    /**
+     * realiza o cálculo do total de ações realizadas
+     * @param {Array} actions lista com as actions encontradas
+     * @return {Float} total total de ações filtradas
+     */
+    getTotalActions(actions) {
+        const total = actions.reduce( (accum, current) => accum + current.total, 0);
+        return total;
+    }
+
+    /**
+     * realiza a listagem de tipos de escola e respectivos totais, em um array de objetos
+     * @param {Array} school_type_id IDs de escolas selecionados
+     * @param {Array} types tipos encontrados na busca
+     * @return void 
+     */
+    listTotalActions(school_type_id, types) {
+        const total_array = [];
+        const tmp_array = [];
+        const action_array = [];
+        const old_state = this.state.action_type_initial;
+        const old_action = this.state.actions_initial;
+
+        school_type_id.map(item => {
+            const actions = types[item.value];
+            const total_actions = this.getTotalActions(actions);  
+            const total_object = { "school_type" : item.value, "total" : total_actions};  
+            total_array.push(total_object);
+            tmp_array.push(actions);
+        });
+
+        tmp_array.map(item => item.map(action => {
+                action_array.push(action);
+            })
+        )
+
+        const total_general = total_array.reduce( (accum, current) => accum + current.total, 0);
+
+        const data_action_type = mapPieChart("Tipos de Escola", "school_type", "total", total_array);
+        const chart_actions = mapPieChart("Ações Realizadas", "action", "total", action_array);
+        this.setState({ data_action_type, data_actions: chart_actions,  total_action: total_general }, function() {
+            if (this.state.data_action_type.length === 1) {
+                this.setState( { data_action_type : old_state, data_actions: old_action });
+                return true;
+            }
+        });
+    }
+
+    /**
+     * obtem o total de ações de acordo com o tipo de escola
+     * @param {String} school_type_id tipo de escola, por exemplo: "público", "particular" ou "secretaria"
+     */
+    getActionsBySchool(school_type_id) {
+        this.setState( { ring_load : true });
+        axios.get("indicator/action/type/school-type")
+        .then(res => {
+            const data_general = res.data.data;
+            const types = data_general.school_type;
+
+            this.listTotalActions(school_type_id, types);
+            
+            this.setState({ ring_load : false });
+        })
+    }
+
+    /**
+     * obtem o total de ações de acordo com o tipo de escola
+     * @param {String} school_type_id tipo de escola, por exemplo: "público", "particular" ou "secretaria"
+     * @param {String} selector seletor a ser atualizado, por exemplo: "total_schools"
+     * @param {String} selector_sum atributo a ser somado, por exemplo: "data_school_type"
+     */
+    sumBySchool(school_type_id, selector, selector_sum) {
+        let general_total = 0;
+        const types = this.state[selector_sum];
+
+        types.map(item => {
+            school_type_id.map(item_filter => {
+                if (item_filter.value.indexOf(item[0]) !== -1) {
+                    general_total = general_total + parseFloat(item[1]);
+                }
+            })
+
+        });
+
+        this.setState( { [selector]: formatNumber(general_total) } );
+    }
+
+    /**
+     * agrupa as categorias e totais de escola
+     */
+    groupBySchool(selected, types, selector) {
+        const final_array = [];
+        final_array[0] = ["LEGENDA", "%"];
+        types.map(item => {
+            selected.map(item_search => {
+                if (item_search.value.indexOf(item[0]) !== -1) {
+                    final_array.push(item);
+                }
+            })
+        })
+
+        console.log(final_array);
+
+
+        if (final_array.length > 1) {
+            this.setState( { [selector] : final_array });
+        }
+
+        
+    }
+
+    handleChangeSchoolType (selectedOption) {
+        const values = this.state;
+        if(selectedOption.length > 0) {
+            values.school_type_id = selectedOption;
+            this.setState({ values }, () => {
+                if (this.state.school_type_id.length === 0) {
+                    this.setState( {
+                        'data_actions': [],
+                        'data_action_type': [],
+                        'data_coverage': [],
+                        'data_action_type': [],
+                        'data_school_type': [],
+                        'data_student_type': [],
+                        'data_student_level': []
+                    });
+    
+                    return true;
+                }
+    
+                this.getActionsBySchool(values.school_type_id);
+                this.sumBySchool(values.school_type_id, "total_schools", "data_school_type");
+                this.sumBySchool(values.school_type_id, "total_students", "data_student_type");
+                this.getCoverage();
+                this.getSchoolTypes();
+                this.getStudentTypes();
+                // this.sumBySchool(values.school_type_id, "total_action", "data_action_type");
+            });
+        }
+
+    }
+
+    getOptions = (input, callback) => {            
+        const select_array = [
+            { "value": "PARTICULAR", "label": "Particular" },
+            { "value": "PUBLICO", "label": "Público" },
+            { "value": "SECRETARIA", "label": "Secretaria" }
+        ];
+
+        this.setState({ search_options: select_array }, function(){
+            setTimeout(() => {
+                callback(null, {
+                options: this.state.search_options,
+                complete: true
+                });
+            }, 500);
+        });
+
+    };
+
     render() {
         const { total_schools, total_students, total_contacts, total_action,
             data_actions, data_school_type, data_student_type, data_action_type, data_student_level,
             data_coverage,
-            options_publisher
+            options_publisher, ring_load
         } = this.state;
 
         return (
             <div>
+                {ring_load == true &&
+                    <div className="loader">
+                        <div className="backLoading">
+                            <div className="load"><img src="https://www.ipswitch.com/library/img/loading.gif" /></div>
+                        </div>
+                    </div>
+                }
+
                 {this.state.msg_error !== '' &&
                     <h4 className="alert alert-danger"> {this.state.msg_error} </h4>
                 }
@@ -285,6 +484,23 @@ class Indicators extends Component {
                         label="Acões"
                     />
 
+                </div>
+                
+                <div className="search-area">
+                    <FormGroup htmlFor="school_type_id">
+                        <label>Tipo de Escola</label>
+                        <Async
+                            removeSelected={false}
+                            name="school_type_id"
+                            id="school_type_id"
+                            disabled={this.state.viewMode}
+                            value={this.state.school_type_id}
+                            onChange={this.handleChangeSchoolType}
+                            loadOptions={this.getOptions}
+                            placeholder="Selecione..."
+                            multi={true}
+                        />
+                    </FormGroup>
                 </div>
 
                 <div className="row">
