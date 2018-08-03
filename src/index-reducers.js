45aaa8ca1665f0fda420abb5697fa87360d89838
Henrique Pessolato <henrique.pessolato@quidde.com.br>
2018-08-03 11:20:29 -0300

    Distribuicao de Carteira em redux

--- src/index-reducers.js
+++ src/index-reducers.js
@@ -4,13 +4,15 @@ import user from './reducers/user'
 import login from './reducers/login'
 import dropdown from './reducers/dropdown'
 import marketshare from './reducers/marketshare'
+import userSchool from './reducers/userSchool'
 
 const IndexReducer = combineReducers({
   user,
   login,
   form,
   dropdown,
-  marketshare
+  marketshare,
+  userSchool
 })
 
 export default IndexReducer  
