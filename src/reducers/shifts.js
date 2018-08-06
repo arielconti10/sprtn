4b3f7576654776212dd559ea22b26ff9d39414ce
Ariel Conti <arielconti10@gmail.com>
2018-08-06 08:46:47 -0300

    .

--- src/reducers/shifts.js
+++ src/reducers/shifts.js
@@ -1,98 +1,150 @@
 import {
-    SHIFT_CREATING,
-    SHIFT_CREATE_SUCCESS,
-    SHIFT_CREATE_ERROR,
-    SHIFT_REQUESTING,
-    SHIFT_REQUEST_SUCCESS,
-    SHIFT_REQUEST_ERROR,
-  } from '../actionTypes/shifts'
-  
-  const initialState = {
-    list: [], // where we'll store shifts
-    requesting: false,
-    successful: false,
-    messages: [],
-    errors: [],
-  }
-  
-  const reducer = function shiftReducer (state = initialState, action) {
-    switch (action.type) {
-      case SHIFT_CREATING:
-        return {
-          ...state,
-          requesting: true,
-          successful: false,
-          messages: [{
-            body: `shift: ${action.shift.name} being created...`,
-            time: new Date(),
-          }],
-          errors: [],
-        }
-  
+  SHIFT_CREATING,
+  SHIFT_CREATE_SUCCESS,
+  SHIFT_CREATE_ERROR,
+  SHIFT_REQUESTING,
+  SHIFT_REQUEST_SUCCESS,
+  SHIFT_LOAD_SUCCESS,
+  SHIFT_LOADING,
+  SHIFT_REQUEST_ERROR,
+  SHIFT_UPDATE_SUCCESS,
+} from '../actionTypes/shifts'
+
+const initialState = {
+  list: [], // where we'll store shifts
+  current_shift: null,
+  requesting: false,
+  successful: false,
+  messages: [],
+  errors: [],
+}
+
+const reducer = function shiftReducer(state = initialState, action) {
+  switch (action.type) {
+    case SHIFT_CREATING:
+      return {
+        ...state,
+        requesting: true,
+        successful: false,
+        messages: [{
+          body: `shift: ${action.shift.name} being created...`,
+          time: new Date(),
+        }],
+        errors: [],
+      }
+
       // On success include the new shift into our list
-      case SHIFT_CREATE_SUCCESS:
-        return {
-          list: state.list.concat([action.shift]),
-          requesting: false,
-          successful: true,
-          messages: [{
-            body: `shift: ${action.shift.name} awesomely created!`,
-            time: new Date(),
-          }],
-          errors: [],
+    case SHIFT_CREATE_SUCCESS:
+      return {
+        list: state.list.concat([action.shift]),
+        requesting: false,
+        successful: true,
+        messages: [{
+          body: `shift: ${action.shift.name} awesomely created!`,
+          time: new Date(),
+        }],
+        errors: [],
+      }
+
+    case SHIFT_CREATING:
+      return {
+        ...state,
+        requesting: true,
+        successful: false,
+        messages: [{
+          body: `shift: ${action.shift.name} being updated...`,
+          time: new Date(),
+        }],
+        errors: [],
+      }
+
+    case SHIFT_UPDATE_SUCCESS:
+      return state.map((shift) => {
+        if (shift.id == action.shift.id) {
+          return {
+            ...shift,
+          }
+        } else {
+          return shift
         }
-  
-      case SHIFT_CREATE_ERROR:
-        return {
-          ...state,
-          requesting: false,
-          successful: false,
-          messages: [],
-          errors: state.errors.concat([{
-            body: action.error.toString(),
-            time: new Date(),
-          }]),
-        }
-  
-      case SHIFT_REQUESTING:
-        return {
-          ...state, // ensure that we don't erase fetched ones
-          requesting: false,
-          successful: true,
-          messages: [{
-            body: 'Fetching shifts...!',
-            time: new Date(),
-          }],
-          errors: [],
-        }
-  
-      case SHIFT_REQUEST_SUCCESS:
-        return {
-          list: action.shifts, // replace with fresh list
-          requesting: false,
-          successful: true,
-          messages: [{
-            body: 'shifts awesomely fetched!',
-            time: new Date(),
-          }],
-          errors: [],
-        }
-  
-      case SHIFT_REQUEST_ERROR:
-        return {
-          requesting: false,
-          successful: false,
-          messages: [],
-          errors: state.errors.concat[{
-            body: action.error.toString(),
-            time: new Date(),
-          }],
-        }
-  
-      default:
-        return state
-    }
+      })
+
+
+    case SHIFT_CREATE_ERROR:
+      return {
+        ...state,
+        requesting: false,
+        successful: false,
+        messages: [],
+        errors: state.errors.concat([{
+          body: action.error.toString(),
+          time: new Date(),
+        }]),
+      }
+
+    case SHIFT_REQUESTING:
+      return {
+        ...state, // ensure that we don't erase fetched ones
+        requesting: false,
+        successful: true,
+        messages: [{
+          body: 'Fetching shifts...!',
+          time: new Date(),
+        }],
+        errors: [],
+      }
+
+    case SHIFT_LOADING:
+      return {
+        ...state,
+        current_shift: null,
+        requesting: false,
+        successful: true,
+        messages: [{
+          body: 'Fetching current shift',
+          time: new Date()
+        }],
+        errors: []
+      }
+
+    case SHIFT_LOAD_SUCCESS:
+      return {
+        current_shift: action.shift.data, // replace with fresh list
+        requesting: false,
+        successful: true,
+        messages: [{
+          body: 'current shift awesomely fetched!',
+          time: new Date(),
+        }],
+        errors: [],
+      }
+
+    case SHIFT_REQUEST_SUCCESS:
+      return {
+        list: action.shifts, // replace with fresh list
+        requesting: false,
+        successful: true,
+        messages: [{
+          body: 'shifts awesomely fetched!',
+          time: new Date(),
+        }],
+        errors: [],
+      }
+
+    case SHIFT_REQUEST_ERROR:
+      return {
+        requesting: false,
+        successful: false,
+        messages: [],
+        errors: state.errors.concat([{
+          body: action.error.toString(),
+          time: new Date(),
+        }]),
+      }
+
+    default:
+      return state
   }
-  
-  export default reducer
-  
+}
+
+export default reducer
--- src/sagas/shifts.js
+++ src/sagas/shifts.js
@@ -1,8 +1,14 @@
-import { call, put, takeLatest } from 'redux-saga/effects'
+import {
+  call,
+  put,
+  takeLatest
+} from 'redux-saga/effects'
 // import { handleApiErrors } from '../lib/api-errors'
 import {
   SHIFT_CREATING,
+  SHIFT_UPDATING,
   SHIFT_REQUESTING,
+  SHIFT_LOADING 
 } from '../actionTypes/shifts'
 
 import {
@@ -10,26 +16,31 @@ import {
   shiftCreateError,
   shiftRequestSuccess,
   shiftRequestError,
+  shiftLoadSuccess,
 } from '../actions/shifts'
 
 const shiftsUrl = `${process.env.API_URL}`
 
 // We'll use this function to redirect to different routes based on cases
-import { createHashHistory } from 'history'
+import {
+  createHashHistory
+} from 'history'
 
 export const history = createHashHistory()
 
 // Nice little helper to deal with the response
 // converting it to json, and handling errors
-function handleRequest (request) {
+function handleRequest(request) {
   return request
     .then(response => response.json())
     .then(json => json)
-    .catch((error) => { throw error })
+    .catch((error) => {
+      throw error
+    })
 }
 
-function shiftCreate (user, shift) {
-  shift.active = true  
+function shiftCreate(user, shift) {
+  shift.active = true
   const url = `${shiftsUrl}/shift`
   const request = fetch(url, {
     method: 'POST',
@@ -47,28 +58,49 @@ function shiftCreate (user, shift) {
   return handleRequest(request)
 }
 
-function* shiftCreateFlow (action) {
-  try {
-    const { user, shift } = action
+function shiftUpdate(user, shift) {
 
-    const createdshift = yield call(shiftCreate, user, shift)
-    // creates the action with the format of
-    // {
-    //   type: SHIFT_CREATE_SUCCESS,
-    //   shift,
-    // }
-    // Which we could do inline here, but again, consistency
+  const url = `${shiftsUrl}/shift/${shift.id}`
+  const request = fetch(url, {
+    method: 'PUT',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': 'http://localhost:9001',
+      // passes our token as an "Authorization" header in
+      // every POST request.
+      Authorization: 'Bearer ' + user.access_token || undefined, // will throw an error if no login
+    },
+    body: JSON.stringify(shift),
+  })
+
+  return handleRequest(request)
+}
+
+function* shiftCreateFlow(action) {
+  try {
+    const {
+      user,
+      shift
+    } = action
+
+    if (shift.id !== undefined) {
+      const updatedShift = yield call(shiftUpdate, user, shift)
+    } else {
+      const createdshift = yield call(shiftCreate, user, shift)
+    }
+
     yield put(shiftCreateSuccess(createdshift))
 
     history.push('/cadastro/turnos')
 
-} catch (error) {
+  } catch (error) {
     // same with error
     yield put(shiftCreateError(error))
   }
 }
 
-function shiftRequest (user) {
+function shiftsRequest(user) {
   const url = `${shiftsUrl}/shifts`
   const request = fetch(url, {
     method: 'GET',
@@ -82,24 +114,64 @@ function shiftRequest (user) {
   return handleRequest(request)
 }
 
-function* shiftRequestFlow (action) {
+function* shiftRequestFlow(action) {
   try {
     // grab the user from our action
-    const { user } = action
-    // call to our shiftRequestApi function with the user
-    const shifts = yield call(shiftRequestApi, user)
+    const {
+      user,
+      shift
+    } = action
+
+    if(shift.id !== undefined){
+      const current_shift = yield call(shiftLoad, user, shift)
+      yield put(shiftLoadSuccess(current_shift))      
+    } else {
+      // call to our shiftRequestApi function with the user
+      const shifts = yield call(shiftsRequest, user)
+      yield put(shiftRequestSuccess(shifts))
+    }
+    
     // dispatch the action with our shifts!
-    yield put(shiftRequestSuccess(shifts))
   } catch (error) {
     yield put(shiftRequestError(error))
   }
 }
 
-function* shiftsWatcher () {
+function* shiftLoadFlow(action) {
+  try {
+    // grab the user from our action
+    const {
+      user,
+      shift
+    } = action
+    
+    const current_shift = yield call(shiftLoad, user, shift)
+    yield put(shiftLoadSuccess(current_shift))
+
+    // dispatch the action with our shifts!
+  } catch (error) {
+    yield put(shiftRequestError(error))
+  }
+}
+
+function shiftLoad(user, shift) {
+  const request = fetch(`${shiftsUrl}/shift/${shift}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        // passe our token as an "Authorization" header
+        Authorization: 'Bearer ' + user.access_token || undefined,
+      },
+    })
+    return handleRequest(request)
+}
+
+function* shiftsWatcher() {
   // each of the below RECEIVES the action from the .. action
   yield [
     takeLatest(SHIFT_CREATING, shiftCreateFlow),
     takeLatest(SHIFT_REQUESTING, shiftRequestFlow),
+    takeLatest(SHIFT_LOADING, shiftLoadFlow),
   ]
 }
 
