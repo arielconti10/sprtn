e9a73febb4f77f9c1af171b1b85404cd71885e9c
Henrique Pessolato <henrique.pessolato@quidde.com.br>
2018-08-16 09:14:21 -0300

    Configurando testes

---
+++ src/tests/sum.test.js
@@ -0,0 +1,19 @@
+const sum = require('./sum');
+
+test('running sum operation', () => {
+    expect(sum(4, 2)).toBe(6);
+});
+
+test('object assignment', () => {
+    const data = {one: 1};
+    data['two'] = 2;
+    expect(data).toEqual({one: 1, two: 2});
+});
+
+test('adding positive numbers is not zero', () => {
+    for (let a = 1; a < 10; a++) {
+      for (let b = 1; b < 10; b++) {
+        expect(a + b).not.toBe(0);
+      }
+    }
+});
