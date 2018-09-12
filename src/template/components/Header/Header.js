c1837b92d482011ecc0aeddeb9f2f8200028afee
Ariel Conti <arielconti10@gmail.com>
2018-09-12 10:09:08 -0300

    Redirect home

--- src/template/components/Header/Header.js
+++ src/template/components/Header/Header.js
@@ -38,7 +38,7 @@ class Header extends Component {
         <NavbarToggler className="d-lg-none" onClick={this.mobileSidebarToggle}>
           <span className="navbar-toggler-icon"></span>
         </NavbarToggler>
-        <NavbarBrand href="#"></NavbarBrand>
+        <NavbarBrand href="#/dashboard/indicadores"></NavbarBrand>
         {/* <NavbarToggler className="d-md-down-none" onClick={this.sidebarToggle}>
           <span className="navbar-toggler-icon"></span>
         </NavbarToggler> */}
